b99908ddf180242f5d7355e847c197a8bc8a10e9
swati22446 <swati22446@gmail.com>
2026-04-11 12:37:40 +0530

    Performed the question with a slight change in loop

--- src/Arrays/CheckIfArrayIsSorted.java
+++ src/Arrays/CheckIfArrayIsSorted.java
@@ -14,6 +14,19 @@ public class CheckIfArrayIsSorted {
         return true;
     }
 
+    boolean isSorted2(int[] nums){
+        int n = nums.length;
+        for(int i =1; i < n; i++){
+            if(nums[i] > nums[i-1]){
+
+            }
+            else{
+                return false;
+            }
+        }
+        return true;
+    }
+
     public static void main(String[] args) {
         CheckIfArrayIsSorted sort = new CheckIfArrayIsSorted();
         int[] arr = {2 , 1 , 2 , 3};
