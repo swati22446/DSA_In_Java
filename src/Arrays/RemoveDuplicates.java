e9bcfed5c0848376bac1cda569c0ea55ac9679e9
swati22446 <swati22446@gmail.com>
2026-04-11 13:39:30 +0530

    My approach using LinkedHashSet in java because it maintains the order of insertion.

--- src/Arrays/RemoveDuplicates.java
+++ src/Arrays/RemoveDuplicates.java
@@ -1,12 +1,12 @@
 package Arrays;
 import java.util.*;
 public class RemoveDuplicates {
-    // My approach
+    // My approach --list approach
     void removeDuplicates(int[] nums){
         int n = nums.length;
         List<Integer> list = new ArrayList<>();
         list.add(nums[0]);
-        for(int i =0; i<n ; i++){
+        for(int i =1; i<n ; i++){
             if(list.contains(nums[i])){
 
             }
@@ -19,10 +19,22 @@ public class RemoveDuplicates {
 
     }
 
+    //Second appraoch -- using set
+    void removeDuplicates2(int[] arr){
+        int n = arr.length;
+        LinkedHashSet<Integer> set = new LinkedHashSet<>();
+        for(int i =0; i < n; i++){
+            set.add(arr[i]);
+        }
+        System.out.println(set);
+    }
+
+
     public static void main(String[] args) {
         RemoveDuplicates rev = new RemoveDuplicates();
         int[] arr = {2, 3 ,2 , 4 , 1};
         rev.removeDuplicates(arr);
+        rev.removeDuplicates2(arr);
 
     }
 }
