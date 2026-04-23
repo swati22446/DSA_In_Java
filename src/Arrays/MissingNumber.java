391d11798ca3586690740bfc90870696ca6584ef
swati22446 <swati22446@gmail.com>
2026-04-23 15:24:52 +0530

    Using better approach by using the sum of n natural numbers

--- src/Arrays/MissingNumber.java
+++ src/Arrays/MissingNumber.java
@@ -1,7 +1,7 @@
 package Arrays;
 
 public class MissingNumber {
-    // my approach
+    // my approach1
     int miss(int[] nums){
         int n = nums.length;
         boolean[] arr = new boolean[n+2];
@@ -15,10 +15,24 @@ public class MissingNumber {
         }
         return 0;
     }
+// optimal approach
+    int miss2(int[] arr){
+        int n = arr.length;
+        int sum = 0;
+        int sumOfArray = 0;
+        for(int i = 1; i <=n+1 ; i++){ // since there has to n+1 elements
+            sum+=i;
+        }
+        for(int i =0; i<n; i++){
+            sumOfArray += arr[i];
+        }
+        return sum-sumOfArray;
+    }
 
     public static void main(String[] args) {
         MissingNumber miss = new MissingNumber();
         int[] arr = {1 , 2, 4 , 5};
         System.out.println(miss.miss(arr));
+        System.out.println(miss.miss2(arr));
     }
 }
