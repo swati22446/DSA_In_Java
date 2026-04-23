d4a6334739f68dc1dddec34f27571de97414cd8c
swati22446 <swati22446@gmail.com>
2026-04-23 12:16:55 +0530

    How Initialization is different from c++

---
+++ src/Arrays/Initialization.java
@@ -0,0 +1,10 @@
+package Arrays;
+import java.util.*;
+
+public class Initialization {
+    public static void main(String[] args) {
+        int[] a = new int[5];
+//        a = {1,2}; // We can't do this like c++ and expect to display. We must asign individually here
+        System.out.println(Arrays.toString(a));
+    }
+}
