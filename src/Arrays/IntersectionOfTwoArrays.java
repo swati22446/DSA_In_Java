f212e0dccf7bfef10fb3efcd3c4dd1fa73b6e733
swati22446 <swati22446@gmail.com>
2026-04-23 14:08:22 +0530

    Intersection using brute force

---
+++ src/Arrays/IntersectionOfTwoArrays.java
@@ -0,0 +1,32 @@
+package Arrays;
+import java.util.*;
+
+public class IntersectionOfTwoArrays {
+    void intersection(int[] arr , int[] nums){
+        int n = arr.length;
+        int m = nums.length;
+        boolean[] vis = new boolean[m];
+        List<Integer> list = new ArrayList<>();
+        for(int i = 0; i < n; i++){
+            for(int j = 0; j < m; j++){
+                if(arr[i] == nums[j] && vis[j] == false){
+                    list.add(arr[i]);
+                    vis[j] = true;
+                    break; // Will only terminate the innermost loop within which it exists
+                }
+                if (nums[j] > arr[i]){
+                    break;
+                }
+            }
+        }
+        System.out.println(list);
+
+    }
+
+    public static void main(String[] args) {
+        IntersectionOfTwoArrays inter = new IntersectionOfTwoArrays();
+        int[] arr1 = {1,2,2,3,4,5};
+        int[] arr2 = {2,3,3,4,5,7,8};
+        inter.intersection(arr1, arr2);
+    }
+}
