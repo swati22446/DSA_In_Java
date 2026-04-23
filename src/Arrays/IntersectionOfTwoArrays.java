f245da7c1eae58f7687b44860449c230d7196739
swati22446 <swati22446@gmail.com>
2026-04-23 14:38:32 +0530

    Using optimal approach

--- src/Arrays/IntersectionOfTwoArrays.java
+++ src/Arrays/IntersectionOfTwoArrays.java
@@ -2,6 +2,7 @@ package Arrays;
 import java.util.*;
 
 public class IntersectionOfTwoArrays {
+    //using brute force
     void intersection(int[] arr , int[] nums){
         int n = arr.length;
         int m = nums.length;
@@ -20,7 +21,28 @@ public class IntersectionOfTwoArrays {
             }
         }
         System.out.println(list);
-
+    }
+    // using optimal solution
+    void intersection2(int[] arr, int[] nums){
+        int n = arr.length;
+        int m = nums.length;
+        int i = 0;
+        int j = 0;
+        List<Integer> list = new ArrayList();
+        while(i < n && j < m){
+            if(arr[i] < nums[j]){
+                i++;
+            }
+            else if(nums[j] < arr[i]){
+                j++;
+            }
+            else{
+                list.add(arr[i]);
+                i++;
+                j++;
+            }
+        }
+        System.out.println(list);
     }
 
     public static void main(String[] args) {
@@ -28,5 +50,6 @@ public class IntersectionOfTwoArrays {
         int[] arr1 = {1,2,2,3,4,5};
         int[] arr2 = {2,3,3,4,5,7,8};
         inter.intersection(arr1, arr2);
+        inter.intersection2(arr1 ,arr2);
     }
 }
