aea0ea5dc54067535715cdefa6e24d5ec510496f
swati22446 <swati22446@gmail.com>
2026-07-06 23:53:22 +0530

    Optimal Approach

--- src/SlidingWindow/MaximumSumSubarray.java
+++ src/SlidingWindow/MaximumSumSubarray.java
@@ -1,6 +1,8 @@
 package SlidingWindow;
 
 public class MaximumSumSubarray {
+
+    //Brute Force
     int maxSum(int[] nums, int k){
         int max = 0;
         int n = nums.length;
@@ -14,11 +16,28 @@ public class MaximumSumSubarray {
         return max;
     }
 
+    // Optimal Approach
+    int maxSum2(int[] nums, int k){
+        int n = nums.length;
+        int currentSum =0;
+        for(int i =0; i<k; i++){
+            currentSum += nums[i];
+        }
+        int max = currentSum;
+        for(int i =0; i<(n-k)+1-1; i++){
+            currentSum-=nums[i];
+            currentSum+=nums[i+k];
+            max = Math.max(currentSum, max);
+        }
+        return max;
+    }
+
 
     public static void main(String[] args) {
         MaximumSumSubarray sub= new MaximumSumSubarray();
         int[] nums = {1 , 4, 1, 10, 25, 3, 5, 0, 26};
-        System.out.println(sub.maxSum(nums,4));
+//        System.out.println(sub.maxSum(nums,4));
+        System.out.println(sub.maxSum2(nums,4));
     }
 }
 
